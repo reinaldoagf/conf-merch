2f7037dab0dcb579ec774eae765ae7b72edca8e9
Reinaldo Gonzalez <reinaldoagf1@gmail.com>
2022-07-03 12:15:08 -0400

    CheckOutContainer

--- src/containers/CheckOut.jsx
+++ src/containers/CheckOut.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import '../styles/components/CheckOut.scss'
 const CheckOut = () => {
     return (
-        <div>
-            CheckOut
+        <div className="Checkout">
+            <div className="Checkout-content">
+                <h3>Lista de pedidos</h3>
+                <div className="Checkout-item">
+                    <div className="Checkout-element">
+                        <h4>Item name</h4>
+                        <span>$20</span>
+                    </div>
+                    <button type='button'>Eliminar</button>
+                </div>
+            </div>
+            <div className="Checkout-sidebar">
+                <h3>Precio total: $20</h3>
+                <button type='button'>Continuar pedido</button>
+            </div>
         </div>
     );
 };
