0f3ef61cf7f52ebdaa19b489a9892a94cb2c018b
Reinaldo Gonzalez <reinaldoagf1@gmail.com>
2022-07-03 01:04:37 -0400

    Layout.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/components/Header.scss'
+const Header = () => {
+    return (
+        <div className='Header'>
+            <h1 className='Header-title'>PlatziConf Merch</h1>
+            <div className='Header-checkout'>Checkout</div>
+        </div>
+    );
+};
+
+export default Header;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,25 +2,24 @@ import React from 'react';
 import {
     BrowserRouter,
     Route,
-    Link,
     Switch,
-    Redirect,
-    useLocation
 } from "react-router-dom";
 
 import { Home, CheckOut, Information, Payment, Success, NotFound } from "../containers"
-
+import Layout from '../components/Layout';
 const app = () => {
     return (
         <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/checkout" component={CheckOut} />
-                <Route exact path="/checkout/information" component={Information} />
-                <Route exact path="/checkout/payment" component={Payment} />
-                <Route exact path="/checkout/success" component={Success} />
-                <Route path="*" component={NotFound} />
-            </Switch>
+            <Layout>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/checkout" component={CheckOut} />
+                    <Route exact path="/checkout/information" component={Information} />
+                    <Route exact path="/checkout/payment" component={Payment} />
+                    <Route exact path="/checkout/success" component={Success} />
+                    <Route path="*" component={NotFound} />
+                </Switch>
+            </Layout>
         </BrowserRouter>
     );
 };
