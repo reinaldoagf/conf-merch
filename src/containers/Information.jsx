1648a505001166d09931313768fd7e6dcf38546f
Reinaldo Gonzalez <reinaldoagf1@gmail.com>
2022-07-03 12:36:11 -0400

    InformationCheckout

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
-
+import '../styles/components/Information.scss';
 const Information = () => {
     return (
-        <div>
-            Information
+        <div className="Information">
+            <div className="Information-content">
+                <div className="Information-head">
+                    <h2>Información de contacto:</h2>
+                </div>
+                <div className="Information-form">
+                    <form action="">
+                        <input type="text" placeholder='Nombre completo' name="name" />
+                        <input type="text" placeholder='Correo Electronico' name="email" />
+                        <input type="text" placeholder='Dirección' name="address" />
+                        <input type="text" placeholder='Apartamento' name="apto" />
+                        <input type="text" placeholder='Ciudad' name="city" />
+                        <input type="text" placeholder='País' name="country" />
+                        <input type="text" placeholder='Estado' name="state" />
+                        <input type="text" placeholder='Codigo postal' name="postal_code" />
+                        <input type="text" placeholder='Telefono' name="phone" />
+                    </form>
+                </div>
+                <div className="Information-buttons">
+                    <div className="Information-back">
+                        Regresar
+                    </div>
+                    <div className="Information-next">
+                        Pagar
+                    </div>
+                </div>
+            </div>
+            <div className="Information-sidebar">
+                <h3>Pedido:</h3>
+                <div className="Information-item">
+                    <div className="Information-element">
+                        <h4>Item name</h4>
+                        <span>$10</span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
