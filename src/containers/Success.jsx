37739482b86049ff1c3f020e19c21f511ea922c9
Reinaldo Gonzalez <reinaldoagf1@gmail.com>
2022-07-08 11:29:51 -0400

    useGoogleAddress

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,17 +1,19 @@
 import React, { useContext } from 'react';
 import AppContext from '../context/AppContext';
 import Map from '../components/Map';
+import useGoogleAddress from '../hooks/useGoogleAddress';
 import '../styles/components/Success.scss';
 const Success = () => {
     const {state}= useContext(AppContext);
     const {buyer} = state;
+    const location = useGoogleAddress(buyer.address)
     return (
         <div className="Success">
             <div className="Success-content">
                 {buyer?.name && <h2>{`${buyer.name}, gracias por tu compra`}</h2> }
                 <span>Tu pedido llegará a 3 días a tu dirección:</span>
                 <div className="Success-map">
-                    <Map />
+                    <Map data={location} />
                 </div>
             </div>
         </div>
